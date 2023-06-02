d55803e437bd53c1eec32e4b8b0fe0ce22f80d69
Sean Luong <sean.luong@joinhonor.com>
2023-06-02 00:09:43 -0400

    Add MUI and create 2 buttons

--- popup.tsx
+++ popup.tsx
@@ -1,27 +1,12 @@
-import { useState } from "react"
+import { Button, Stack } from "@mui/material";
+
 
 function IndexPopup() {
-  const [data, setData] = useState("")
-
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
+    <Stack direction='row' gap={2}>
+      <Button variant="contained" color="primary">Disemvowel</Button>
+      <Button variant="outlined" color="secondary">Reset</Button>
+    </Stack>
   )
 }
 
