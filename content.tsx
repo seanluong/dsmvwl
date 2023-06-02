81c43668ed214c3b211ea338689c139847622610
Sean Luong <sean.luong@joinhonor.com>
2023-06-02 01:46:02 -0400

    Implement the logic

---
+++ content.tsx
@@ -0,0 +1,49 @@
+import { useMessage } from "@plasmohq/messaging/hook"
+import { useState } from "react";
+
+const VOWELS = new Set(["a", "e", "i", "o", "u"]);
+
+const removeVowels = (text: string): string => {
+    return text.split("").filter(char => !VOWELS.has(char.toLowerCase())).join("");
+}
+
+const disemvowel = (node: Node, ancestors: number[], lookup: Map<string, string>) => {
+    if (node.nodeType === Node.TEXT_NODE) {
+        const nodeId = ancestors.join(".");
+        if (!lookup.has(nodeId)) {
+            lookup.set(nodeId, node.textContent);
+        }
+        node.textContent = removeVowels(node.textContent);
+    } else {
+        node.childNodes.forEach((child, key) => disemvowel(child, [...ancestors, key], lookup));
+    }
+};
+
+const reset = (node: Node, ancestors: number[], lookup: Map<string, string>) => {
+    if (node.nodeType === Node.TEXT_NODE) {
+        const nodeId = ancestors.join(".");
+        node.textContent = lookup.get(nodeId) ?? "";
+    } else {
+        node.childNodes.forEach((child, key) => reset(child, [...ancestors, key], lookup));
+    }
+}
+
+const Disemvowel = () => {
+    const [lookup, setLookup] = useState<Map<string, string>>(new Map<string, string>());
+    useMessage<string, string>(async (req, res) => {
+        const {name} = req;
+        if (name === "disemvowel") {
+            disemvowel(document.body, [], lookup);
+            setLookup(lookup);
+            res.send("disemvoweled");
+        } else if (name === "reset") {
+            if (lookup !== null) {
+                reset(document.body, [], lookup);
+                res.send("reset");
+            }
+        }
+    })
+    return null;
+}
+
+export default Disemvowel;
--- popup.tsx
+++ popup.tsx
@@ -1,11 +1,24 @@
 import { Button, Stack } from "@mui/material";
-
+import { sendToContentScript } from "@plasmohq/messaging"
+import { useState } from "react";
 
 function IndexPopup() {
+  const [disemvoweled, setDisemvoweled] = useState(false);
+
+  async function handleClick(command) {
+    const resp = await sendToContentScript({ name: command });
+    console.log(resp);
+    if (resp === 'disemvoweled') {
+      setDisemvoweled(true);
+    } else if (resp === 'reset') {
+      setDisemvoweled(false);
+    }
+  }
+
   return (
     <Stack direction='row' gap={2}>
-      <Button variant="contained" color="primary">Disemvowel</Button>
-      <Button variant="outlined" color="secondary">Reset</Button>
+      <Button disabled={disemvoweled} variant="contained" color="primary" onClick={() => handleClick("disemvowel")}>Disemvowel</Button>
+      <Button disabled={!disemvoweled} variant="contained" color="secondary" onClick={() => handleClick("reset")}>Reset</Button>
     </Stack>
   )
 }
